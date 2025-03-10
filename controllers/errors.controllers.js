167d103fdf63857717f07bc58dc50592e9938f3d
JamesU169 <jamesuczniak@outlook.com>
2025-03-10 14:12:11 +0000

    Successfully tests for and fetches topics. Error handlers also created

---
+++ controllers/errors.controllers.js
@@ -0,0 +1,18 @@
+exports.handlePsqlErrors = (error, request, response, next) => {
+    if (error.code === '22P02') {
+        response.status(400).send({ msg: "400 Bad Request"});
+    }
+    next(error);
+}
+
+exports.handleCustomErrors = (error, request, response, next) => {
+    if (error.status) {
+        response.status(error.status).send({ msg: error.msg});
+    }
+    next(error);
+}
+
+exports.handleServerErrors = (error, request, response, next) => {
+    console.error(error);
+    response.status(500).send({msg: "500 Internal Server Error"})
+}
--- controllers/ncnews.controllers.js
+++ controllers/ncnews.controllers.js
@@ -1,5 +1,12 @@
 const endpoints = require("../endpoints.json");
+const { fetchTopics } = require("../models/ncnews.models");
 
 exports.getEndpoints = (request, response) => {
     response.status(200).json({endpoints});
 }
+
+exports.getTopics = (request, response) => {
+    fetchTopics().then((topics) => {
+        response.status(200).json({ topics })
+    });
+};

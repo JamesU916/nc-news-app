21c9f5a7cceeb9a024c625959a0963ff03b0b25e
JamesU169 <jamesuczniak@outlook.com>
2025-03-11 14:54:42 +0000

    Changes made to account for empty comment objects. Tests also added and refactored

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,10 @@
 const endpointsJson = require("../endpoints.json")
-/* Set up your test imports here */
 const app = require("../app");
 const data = require("../db/data/test-data");
 const db = (require("../db/connection"))
 const request = require("supertest");
 const seed = require("../db/seeds/seed");
 
-/* Set up your beforeEach & afterAll functions here */
 beforeEach(() => {
   return seed(data)
 });
@@ -64,13 +62,21 @@ describe("GET /api/articles", () => {
       const articles = body.articles;
       articles.forEach((article) => {
         expect(typeof article.author).toBe("string")
+        expect(article.author.length).not.toBe(0)
         expect(typeof article.title).toBe("string")
+        expect(article.title.length).not.toBe(0)
         expect(typeof article.article_id).toBe("number")
+        expect(article.article_id.length).not.toBe(0)
         expect(typeof article.topic).toBe("string")
+        expect(article.topic.length).not.toBe(0)
         expect(typeof article.created_at).toBe("string")
+        expect(article.created_at.length).not.toBe(0)
         expect(typeof article.votes).toBe("number")
+        expect(article.votes.length).not.toBe(0)
         expect(typeof article.article_img_url).toBe("string")
+        expect(article.article_img_url.length).not.toBe(0)
         expect(typeof article.comment_count).toBe("string")
+        expect(article.comment_count.length).not.toBe(0)
       })
       expect(articles).toBeSortedBy("created_at", { descending: true});
     })
@@ -84,14 +90,16 @@ describe("GET /api/articles/:article_id", () => {
     .expect(200)
     .then(({ body }) => {
       const article = body.article;
-      expect(article.author).toBe("icellusedkars")
-      expect(article.title).toBe("Sony Vaio; or, The Laptop")
-      expect(article.article_id).toBe(2)
-      expect(article.body).toBe("Call me Mitchell. Some years ago..")
-      expect(article.topic).toBe("mitch")
-      expect(article.created_at).toBe("2020-10-16T05:03:00.000Z")
-      expect(article.votes).toBe(0)
-      expect(article.article_img_url).toBe("https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700")
+      expect(article).toMatchObject({
+        author: "icellusedkars",
+        title: "Sony Vaio; or, The Laptop",
+        article_id: 2,
+        body: "Call me Mitchell. Some years ago..",
+        topic: "mitch",
+        created_at: "2020-10-16T05:03:00.000Z",
+        votes: 0,
+        article_img_url: "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+      })
     })
   })
   test("400: Responds with an error when a non-numerical value is passed as a query", () => {
@@ -121,14 +129,30 @@ describe("GET /api/articles/:article_id/comments", () => {
       const comments = body.comments;
       comments.forEach((comment) => {
         expect(typeof comment.comment_id).toBe("number")
+        expect(comment.comment_id.length).not.toBe(0)
         expect(typeof comment.votes).toBe("number")
+        expect(comment.votes.length).not.toBe(0)
         expect(typeof comment.created_at).toBe("string")
+        expect(comment.created_at.length).not.toBe(0)
         expect(typeof comment.author).toBe("string")
+        expect(comment.author.length).not.toBe(0)
         expect(typeof comment.body).toBe("string")
+        expect(comment.body.length).not.toBe(0)
         expect(typeof comment.article_id).toBe("number")
+        expect(comment.article_id.length).not.toBe(0)
         expect(comment.article_id).toBe(1)
       })
       expect(comments).toBeSortedBy("created_at", { descending: true});
+    })
+  })
+  test("200: Responds with an empty array where an article exists but has no comments", () => {
+    return request(app)
+    .get("/api/articles/4/comments")
+    .expect(200)
+    .then(({ body }) => {
+      comments = body.comments;
+      expect(comments.length).toBe(0)
+      expect(comments).toBeArray();
     })
   })
   test("400: Responds with an error when a non-numerical value is passed as a query", () => {
--- models/ncnews.models.js
+++ models/ncnews.models.js
@@ -25,13 +25,16 @@ exports.fetchArticleById = (id) => {
 }
 
 exports.fetchArticleCommentsById = (id) => {
-    return db.query(`SELECT comments.comment_id, comments.votes, comments.created_at, comments.author, comments.body, comments.article_id FROM comments 
-        JOIN articles ON comments.article_id = articles.article_id WHERE comments.article_id = $1 ORDER BY comments.created_at DESC`, [id])
-        .then (({ rows }) => {
+    return db.query(`SELECT * FROM articles WHERE article_id = $1`, [id])
+        .then(({rows}) => {
             if (rows.length === 0) {
                 return Promise.reject({status: 404, msg: "404 Not Found"})
             }
-        return rows;
+            return db.query(`SELECT comments.comment_id, comments.votes, comments.created_at, comments.author, comments.body, comments.article_id FROM comments 
+                JOIN articles ON comments.article_id = articles.article_id WHERE comments.article_id = $1 ORDER BY comments.created_at DESC`, [id])
+                .then (({ rows }) => {
+                return rows;
+            })
         })
 }
 

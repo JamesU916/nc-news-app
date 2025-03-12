eaaa148deab2628c9b08752e3b1aea9aa11d7b60
JamesU169 <jamesuczniak@outlook.com>
2025-03-12 10:23:09 +0000

    Added & tested function to delete comments by their ID

--- models/ncnews.models.js
+++ models/ncnews.models.js
@@ -55,3 +55,12 @@ exports.addArticleVotesById = (inc_votes, article_id) => {
         return rows[0]
     })
 }
+
+exports.removeCommentById = (comment_id) => {
+    return db.query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [comment_id])
+    .then(({ rows }) => {
+        if(rows.length === 0) {
+            return Promise.reject({ status: 404, msg: "404 Not Found - Comment does not exist"})
+        }
+    })
+}

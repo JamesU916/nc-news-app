4eaa73dda762fbc30efaf1f3e152350ce6540f7f
JamesU169 <jamesuczniak@outlook.com>
2025-03-13 12:00:51 +0000

    Small changes to tidy up connection.js file

--- db/connection.js
+++ db/connection.js
@@ -2,12 +2,7 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || 'development'
 
-
-
-
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
-
-// const db = new Pool();
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("PGDATABASE or DATABASE_URL not set")
@@ -21,7 +16,5 @@ if(ENV === "production") {
     config.connectionString = process.env.DATABASE_URL;
     config.max = 2;
 }
-
-// module.exports = db;
 
 module.exports = new Pool(config);
